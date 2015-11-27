95350e78c388bb16e3bfba845c375ae614bd0c14
pawelchojnacki <chojnacki.pawel@linux.com>
2015-11-27 12:35:34 +0100

    add gulp-plumber to handle watch errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var serve = require('gulp-serve')
+var serve = require('gulp-serve');
+var plumber = require('gulp-plumber');
 
 var ES6_SOURCE = 'es6/**/*';
 var JS_TARGET = 'js';
 
 gulp.task('js', function () {
   return gulp.src(ES6_SOURCE)
-  .pipe(babel())
-  .pipe(gulp.dest(JS_TARGET))
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest(JS_TARGET))
 })
 
 gulp.task('watch', function () {
