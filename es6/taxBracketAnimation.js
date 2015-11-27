e48d05f4672c9ed0c6eddd4121318fa6de398e23
pawelchojnacki <chojnacki.pawel@linux.com>
2015-11-27 12:27:56 +0100

    second part of the tutorial - svg

--- es6/taxBracketAnimation.js
+++ es6/taxBracketAnimation.js
@@ -2,13 +2,29 @@
 
 let data = [4, 8, 15, 16, 23, 43];
 
-let scaleX = d3.scale.linear() /* returns a function */
-  .domain([0, d3.max(data)])
-  .range([0, 420]);
+let width = 420,
+    barHeight = 20;
 
-d3.select('.chart')
-  .selectAll('div')
+let x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
+
+let chart = d3.select('.chart')
+    .attr('width', width)
+    .attr('height', barHeight * data.length);
+
+let bar = chart.selectAll('g')
     .data(data)
-  .enter().append('div')
-    .style('width', datapoint => scaleX(datapoint) + 'px')
-    .text(datapoint => datapoint);
+    .enter().append('g')
+    .attr('transform',
+      (d, i) => 'translate(0,' + i * barHeight + ')');
+
+bar.append('rect')
+    .attr('width', x)
+    .attr('height', barHeight -1);
+
+bar.append('text')
+    .attr('x', d => x(d) -3)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.35em')
+    .text(d => d);
--- js/taxBracketAnimation.js
+++ js/taxBracketAnimation.js
@@ -4,11 +4,21 @@
 
 var data = [4, 8, 15, 16, 23, 43];
 
-var scaleX = d3.scale.linear() /* returns a function */
-.domain([0, d3.max(data)]).range([0, 420]);
+var width = 420,
+    barHeight = 20;
 
-d3.select('.chart').selectAll('div').data(data).enter().append('div').style('width', function (datapoint) {
-  return scaleX(datapoint) + 'px';
-}).text(function (datapoint) {
-  return datapoint;
+var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, width]);
+
+var chart = d3.select('.chart').attr('width', width).attr('height', barHeight * data.length);
+
+var bar = chart.selectAll('g').data(data).enter().append('g').attr('transform', function (d, i) {
+    return 'translate(0,' + i * barHeight + ')';
+});
+
+bar.append('rect').attr('width', x).attr('height', barHeight - 1);
+
+bar.append('text').attr('x', function (d) {
+    return x(d) - 3;
+}).attr('y', barHeight / 2).attr('dy', '.35em').text(function (d) {
+    return d;
 });
