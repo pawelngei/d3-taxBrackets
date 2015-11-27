f5e20477e3d79ff5727ac29e4e782d58ef344393
pawelchojnacki <chojnacki.pawel@linux.com>
2015-11-27 15:19:14 +0100

    working data imports

--- es6/taxBracketAnimation.js
+++ es6/taxBracketAnimation.js
@@ -6,25 +6,34 @@ let width = 420,
     barHeight = 20;
 
 let x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+   .range([0, width]);
 
 let chart = d3.select('.chart')
     .attr('width', width)
-    .attr('height', barHeight * data.length);
 
-let bar = chart.selectAll('g')
-    .data(data)
-    .enter().append('g')
-    .attr('transform',
-      (d, i) => 'translate(0,' + i * barHeight + ')');
+d3.csv('data.csv', type, function (error, data) {
+  x.domain([0, d3.max(data, d => d.value)]);
 
-bar.append('rect')
-    .attr('width', x)
-    .attr('height', barHeight -1);
+  chart.attr('height', barHeight * data.length);
 
-bar.append('text')
-    .attr('x', d => x(d) -3)
-    .attr('y', barHeight / 2)
-    .attr('dy', '.35em')
-    .text(d => d);
+  let bar = chart.selectAll('g')
+      .data(data)
+      .enter().append('g')
+      .attr('transform',
+        (d, i) => 'translate(0,' + i * barHeight + ')');
+
+  bar.append('rect')
+      .attr('width', d => x(d.value))
+      .attr('height', barHeight -1);
+
+  bar.append('text')
+      .attr('x', d => x(d.value) -3)
+      .attr('y', barHeight / 2)
+      .attr('dy', '.35em')
+      .text(d => d.value);
+});
+
+function type (d) {
+  d.value = +d.value;
+  return d;
+}
