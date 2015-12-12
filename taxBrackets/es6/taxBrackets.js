31a971635f504555a634db437e95eab1f5809141
pawelchojnacki <chojnacki.pawel@linux.com>
2015-12-12 22:57:24 +0100

    first draft of a d3 app

--- taxBrackets/es6/taxBrackets.js
+++ taxBrackets/es6/taxBrackets.js
@@ -15,8 +15,18 @@ let taxData = [
     constant: 0
   },
   {
-    limit: 20000,
+    limit: 20833,
     taxValue: 44,
+    constant: 0
+  },
+  {
+    limit: 41667,
+    taxValue: 55,
+    constant: 0
+  },
+  {
+    limit: -1, /* inf */
+    taxValue: 75,
     constant: 0
   }
 ];
@@ -26,5 +36,33 @@ let salary = 25000,
 
 let outerWidth = 1000,
     outerHeight = 200,
-    margin = { top: 0, right: 0, bottom: 0, left: 0};
+    margin = { top: 0, right: 25, bottom: 0, left: 25},
+    innerWidth = outerWidth - margin.left - margin.right,
+    innerHeight = outerHeight - margin.top - margin.bottom;
+
+let xScale = d3.scale.linear().range([0, innerWidth])
+      .domain([0,salary]);
+
+let svg = d3.select('#taxBrackets').append('svg')
+    .attr('width', outerWidth)
+    .attr('height', outerHeight);
+
+let frame = svg.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+
+
+frame.append('rect')
+    .attr('x', 0)
+    .attr('y', 150)
+    .attr('width', xScale(salary))
+    .attr('height', 25)
+    .attr('class', 'salary');
+
+frame.append('rect')
+    .attr('x', 0)
+    .attr('y', 125)
+    .attr('width', xScale(exampleTax))
+    .attr('height', 25)
+    .attr('class', 'tax');
 
--- taxBrackets/js/taxBrackets.js
+++ taxBrackets/js/taxBrackets.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var taxData = [{
   limit: 1000,
@@ -13,8 +13,16 @@ var taxData = [{
   taxValue: 33,
   constant: 0
 }, {
-  limit: 20000,
+  limit: 20833,
   taxValue: 44,
+  constant: 0
+}, {
+  limit: 41667,
+  taxValue: 55,
+  constant: 0
+}, {
+  limit: -1, /* inf */
+  taxValue: 75,
   constant: 0
 }];
 
@@ -23,4 +31,16 @@ var salary = 25000,
 
 var outerWidth = 1000,
     outerHeight = 200,
-    margin = { top: 0, right: 0, bottom: 0, left: 0 };
+    margin = { top: 0, right: 25, bottom: 0, left: 25 },
+    innerWidth = outerWidth - margin.left - margin.right,
+    innerHeight = outerHeight - margin.top - margin.bottom;
+
+var xScale = d3.scale.linear().range([0, innerWidth]).domain([0, salary]);
+
+var svg = d3.select('#taxBrackets').append('svg').attr('width', outerWidth).attr('height', outerHeight);
+
+var frame = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+frame.append('rect').attr('x', 0).attr('y', 150).attr('width', xScale(salary)).attr('height', 25).attr('class', 'salary');
+
+frame.append('rect').attr('x', 0).attr('y', 125).attr('width', xScale(exampleTax)).attr('height', 25).attr('class', 'tax');
